3f2e41754396c3e57c838595f26497db57128653
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2023-06-05 01:11:33 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,11 +27,7 @@ export const metadata: Metadata = {
   }
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ja'>
       <body className={`${notoSansJp.variable} font-notosansjp`}>
