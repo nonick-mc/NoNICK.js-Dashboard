7f9647a6432cdb0e404a1dc729ada985c8339986
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2023-07-09 23:20:38 +0900

    雛形の作成

---
+++ src/config/site.ts
@@ -0,0 +1,9 @@
+import { SiteConfig } from '@/types';
+
+export const siteConfig: SiteConfig = {
+  metadata: {
+    name: 'NoNICK.js',
+    description: 'Discordサーバーの管理をサポートする多機能BOT',
+    url: 'http://localhost:3000',
+  }
+}
