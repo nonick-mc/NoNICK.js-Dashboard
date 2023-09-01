18f03f4c9e21de3ba5d6e2fac59fa231c5dd2cb4
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2023-09-01 13:26:14 +0900

    hotfix: ReadonlyHeadersError

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -2,21 +2,23 @@ import { Metadata } from 'next';
 import { PartialGuild } from '@/types/discord';
 import { GuildTable } from './table';
 import { headers } from 'next/headers';
+import { ReadonlyHeaders } from 'next/dist/server/web/spec-extension/adapters/headers';
 
 export const metadata: Metadata = {
   title: 'サーバー選択'
 }
 
-async function getMutualGuilds() {
+async function getMutualGuilds(header: ReadonlyHeaders) {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/guilds`,
-    { headers: headers() }
+    { headers: header }
   );
   return await res.json<PartialGuild[]>();
 }
 
 export default async function Page() {
-  const guilds = await getMutualGuilds();
+  const headerList = headers();
+  const guilds = await getMutualGuilds(headerList);
 
   return (
     <main className='container space-y-8 py-3'>
