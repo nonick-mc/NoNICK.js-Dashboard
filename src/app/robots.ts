e441dbf1db10e0e778b77c7a69284afb2bbb720b
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2023-11-07 18:40:32 +0900

    add: メタデータ系の移植

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import metadataConfig from '@/config/metadata';
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/dashboard',
+    },
+    sitemap: `${metadataConfig.url}/sitemap.xml`,
+  };
+}
