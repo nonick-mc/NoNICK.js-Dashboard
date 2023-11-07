d1181462f1fbf7500b23bcde05780d6554cee4ee
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2023-11-07 18:35:26 +0900

    add: ダークモード

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
-import type { Config } from 'tailwindcss'
-import { nextui } from '@nextui-org/react'
+import type { Config } from 'tailwindcss';
+import { nextui } from '@nextui-org/react';
 
 const config: Config = {
   content: [
@@ -12,12 +12,11 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   darkMode: 'class',
   plugins: [nextui()],
-}
-export default config
+};
+export default config;
