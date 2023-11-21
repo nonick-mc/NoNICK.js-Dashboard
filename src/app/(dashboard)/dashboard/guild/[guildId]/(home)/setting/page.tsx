b9f9e87bd0715e42cb28a78a2d6082fa0ae4b2d8
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2023-11-22 02:20:03 +0900

    add: 全般設定

---
+++ src/app/(dashboard)/dashboard/guild/[guildId]/(home)/setting/page.tsx
@@ -0,0 +1,23 @@
+import { getChannels } from '@/lib/discord';
+import { Metadata } from 'next';
+import { Header } from '../../header';
+import { Alert, AlertTitle } from '@/components/ui/alert';
+import { Form } from './form';
+
+export const metadata: Metadata = {
+  title: '設定',
+};
+
+export default async function Page({ params: { guildId } }: { params: { guildId: string } }) {
+  const channels = await getChannels(guildId);
+
+  return (
+    <>
+      <Header title='設定' description='このサーバーでのNoNICK.jsの全般設定を変更します。' />
+      <Alert variant='primary'>
+        <AlertTitle>💡 この設定はv5.0から行えるようになります。</AlertTitle>
+      </Alert>
+      <Form channels={channels} />
+    </>
+  );
+}
---
+++ src/app/(dashboard)/dashboard/guild/[guildId]/classnames.tsx
@@ -0,0 +1,9 @@
+export const switchClassNames = {
+  base: 'max-w-none flex-row-reverse justify-between gap-3',
+  label: 'text-sm',
+};
+
+export const selectClassNames = {
+  base: 'items-center justify-between',
+  mainWrapper: 'max-w-xs',
+};
