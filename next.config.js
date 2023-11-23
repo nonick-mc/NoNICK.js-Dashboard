1734d18ca3c9be126a4bb245d3d0f6202deb25f7
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2023-11-23 14:08:16 +0900

    add: PWAアプリの対応

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   images: {
     remotePatterns: [
       {
@@ -8,6 +15,6 @@ const nextConfig = {
       },
     ],
   },
-};
+});
 
 module.exports = nextConfig;
