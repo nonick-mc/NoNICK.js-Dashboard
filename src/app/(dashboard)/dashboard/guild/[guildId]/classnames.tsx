801d78d35fa2208522711aabfc747be6fd712034
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-01-10 13:48:29 +0900

    init

--- src/app/(dashboard)/dashboard/guild/[guildId]/classnames.tsx
+++
@@ -1,9 +0,0 @@
-export const switchClassNames = {
-  base: 'max-w-none flex-row-reverse justify-between gap-3',
-  label: 'text-sm',
-};
-
-export const selectClassNames = {
-  base: 'items-center justify-between',
-  mainWrapper: 'max-w-xs',
-};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,34 +1,20 @@
-import type { Config } from 'tailwindcss';
-import tailwindAnimate from 'tailwindcss-animate';
-import { nextui } from '@nextui-org/react';
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
-    },
     extend: {
-      fontFamily: {
-        notoSansJP: ['--font-notoSansJP'],
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  darkMode: 'class',
-  plugins: [
-    nextui({
-      addCommonColors: true,
-    }),
-    tailwindAnimate,
-  ],
-};
-export default config;
+  plugins: [],
+}
+export default config
