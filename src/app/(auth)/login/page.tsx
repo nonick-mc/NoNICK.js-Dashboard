53591f9cdd8894e46c6a3f7baf447bb226b064fb
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-01-10 21:05:44 +0900

    Add login form and page components

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,51 @@
+import Logo from '@/components/logo';
+import { Card, CardBody, CardHeader } from '@nextui-org/card';
+import { Link } from '@nextui-org/link';
+import { Metadata } from 'next';
+import { UserAuthForm } from './form';
+
+export const metadata: Metadata = {
+  title: 'ログイン',
+};
+
+export default function Page() {
+  return (
+    <div className='container flex h-dvh items-center justify-center'>
+      <Card className='w-[400px] space-y-6'>
+        <CardHeader className='flex flex-col items-start gap-6 p-6 pb-0'>
+          <Logo className='pt-3' width={140} />
+          <section>
+            <h1 className='text-xl font-medium'>ログインが必要です</h1>
+            <p className='text-sm text-foreground-500'>
+              Discordアカウントを使用して続行
+            </p>
+          </section>
+        </CardHeader>
+        <CardBody className='flex flex-col gap-6 p-6 pt-0'>
+          <UserAuthForm />
+          <p className='text-sm text-foreground-500'>
+            ログインすることで、NoNICK.jsの
+            <Link
+              className='text-sm'
+              href='https://docs.nonick-js.com/important/teams-of-service/'
+              isExternal
+              showAnchorIcon
+            >
+              利用規約
+            </Link>
+            および
+            <Link
+              className='text-sm'
+              href='https://docs.nonick-js.com/important/privacy-policy/'
+              isExternal
+              showAnchorIcon
+            >
+              プライバシーポリシー
+            </Link>
+            に同意したとみなされます。
+          </p>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -2,11 +2,14 @@
 
 import { NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider } from 'next-themes';
+import { useRouter } from 'next/navigation';
 import { ReactNode } from 'react';
 
 export function Providers({ children }: { children: ReactNode }) {
+  const router = useRouter();
+
   return (
-    <NextUIProvider>
+    <NextUIProvider navigate={router.push}>
       <ThemeProvider attribute='class' defaultTheme='dark'>
         {children}
       </ThemeProvider>
