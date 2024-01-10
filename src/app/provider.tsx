281306ef49fbd5b70cf330ba5b9a975603e140cc
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-01-10 14:12:30 +0900

    Add next-themes package and update tailwind.config.ts

---
+++ src/app/provider.tsx
@@ -0,0 +1,13 @@
+import { NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider } from 'next-themes';
+import { ReactNode } from 'react';
+
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <NextUIProvider>
+      <ThemeProvider attribute='class' defaultTheme='dark'>
+        {children}
+      </ThemeProvider>
+    </NextUIProvider>
+  );
+}
