8cb206d2f923397b9e83551ad60d7933b01f65b5
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-01-10 20:43:31 +0900

    Add NextAuth for authentication

---
+++ src/api/auth/[...nextauth]/auth.ts
@@ -0,0 +1,38 @@
+import { NextAuthOptions } from 'next-auth';
+import DiscordProvider from 'next-auth/providers/discord';
+
+export const authOption: NextAuthOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.NEXT_PUBLIC_DISCORD_ID,
+      clientSecret: process.env.DISCORD_OAUTH_SECRET,
+      authorization: { params: { scope: process.env.DISCORD_OAUTH_SCOPE } },
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    jwt: async ({ token, account }) => {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.accessTokenExpires = account.expires_at;
+      }
+
+      if (
+        token.accessTokenExpires &&
+        Date.now() > token.accessTokenExpires * 1000
+      ) {
+        token.error = 'invalid_token';
+      }
+
+      return token;
+    },
+    session: async ({ session, token }) => {
+      session.accessToken = token.accessToken;
+      session.error = token.error;
+      return session;
+    },
+  },
+};
