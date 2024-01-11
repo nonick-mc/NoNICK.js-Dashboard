b904a51976861b237d41638d535bb10a84364fe8
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-01-11 14:12:54 +0900

    Add next-pwa package and configure next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA({
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+      },
+    ],
+  },
+});
+
+module.exports = nextConfig;
