d5142e6bac68f1a3f785522399349b4e26fdb6ad
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-01-15 02:35:07 +0900

    Refactor loading component names

--- src/app/(dashboard)/dashboard/guild/[guildId]/loading.tsx
+++ src/app/(dashboard)/dashboard/guild/[guildId]/loading.tsx
@@ -1,10 +1,8 @@
 import { Spinner } from '@nextui-org/spinner';
-import { ToolbarMockup } from './toolbar-mockup';
 
 export default function LoadingPage() {
   return (
-    <div className='py-6 flex flex-col gap-6'>
-      <ToolbarMockup />
+    <div className='flex w-full justify-center items-center'>
       <Spinner color='primary' size='md' />
     </div>
   );
