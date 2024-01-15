c741d871fed79d2bfe3fa0af1ec51a52621c41ba
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-01-15 12:41:30 +0900

    Add Header component to guild page

---
+++ src/app/(dashboard)/dashboard/guild/[guildId]/header.tsx
@@ -0,0 +1,11 @@
+export function Header({
+  title,
+  description,
+}: { title: string; description?: string }) {
+  return (
+    <div className='flex flex-col gap-1'>
+      <h1 className='text-3xl font-black'>{title}</h1>
+      {description && <h2 className='text-default-500'>{description}</h2>}
+    </div>
+  );
+}
