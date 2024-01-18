5cfd9a93240d0c74bbfc3c90259d28fbbf849dc9
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-01-19 01:49:41 +0900

    Update description font size in header component

--- src/app/(dashboard)/dashboard/guild/[guildId]/header.tsx
+++ src/app/(dashboard)/dashboard/guild/[guildId]/header.tsx
@@ -5,7 +5,9 @@ export function Header({
   return (
     <div className='flex flex-col gap-1'>
       <h1 className='text-3xl font-black'>{title}</h1>
-      {description && <h2 className='text-default-500'>{description}</h2>}
+      {description && (
+        <h2 className='max-sm:text-sm text-default-500'>{description}</h2>
+      )}
     </div>
   );
 }
