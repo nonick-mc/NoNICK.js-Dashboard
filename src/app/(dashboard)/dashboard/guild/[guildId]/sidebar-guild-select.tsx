3c508aec56ba448c0ed8d459c352c80473e02f05
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2024-01-19 18:50:04 +0900

    Update sidebar-guild-select to handle empty value in onChange event

--- src/app/(dashboard)/dashboard/guild/[guildId]/sidebar-guild-select.tsx
+++ src/app/(dashboard)/dashboard/guild/[guildId]/sidebar-guild-select.tsx
@@ -6,6 +6,7 @@ import { Avatar } from '@nextui-org/react';
 import { Select, SelectItem, SelectedItems } from '@nextui-org/select';
 import { RESTAPIPartialCurrentUserGuild } from 'discord-api-types/v10';
 import { useParams } from 'next/navigation';
+import { Key } from 'react';
 
 type Props = {
   mutualGuilds: RESTAPIPartialCurrentUserGuild[];
@@ -15,6 +16,16 @@ export function SidebarGuildSelect({ mutualGuilds }: Props) {
   const router = usePRouter();
   const guildId = useParams().guildId as string;
 
+  function renderValue(items: SelectedItems<RESTAPIPartialCurrentUserGuild>) {
+    return (
+      <div className='flex flex-wrap items-center'>
+        {items.map((item) => (
+          <SingleSelectItem guild={item.data} key={item.data?.id} />
+        ))}
+      </div>
+    );
+  }
+
   return (
     <Select
       onChange={(e) => router.push(`/dashboard/guild/${e.target.value}`)}
@@ -23,44 +34,34 @@ export function SidebarGuildSelect({ mutualGuilds }: Props) {
       size='md'
       variant='bordered'
       aria-label='サーバー選択'
-      renderValue={(items: SelectedItems<RESTAPIPartialCurrentUserGuild>) => (
-        <>
-          {items.map((item) => (
-            <div
-              className='flex items-center gap-3 text-foreground'
-              key={item.key}
-            >
-              <Avatar
-                src={
-                  item.data?.icon
-                    ? `${Discord.Endpoints.CDN}/icons/${item.data?.id}/${item.data?.icon}.webp`
-                    : `${Discord.Endpoints.CDN}/embed/avatars/0.png`
-                }
-                size='sm'
-                aria-label='サーバーアイコン'
-              />
-              <span className='font-semibold'>{item.data?.name}</span>
-            </div>
-          ))}
-        </>
-      )}
+      renderValue={renderValue}
+      disallowEmptySelection
     >
       {(guild) => (
         <SelectItem key={guild.id} value={guild.id} textValue={guild.name}>
-          <div className='flex items-center gap-3'>
-            <Avatar
-              src={
-                guild.icon
-                  ? `${Discord.Endpoints.CDN}/icons/${guild.id}/${guild.icon}.webp`
-                  : `${Discord.Endpoints.CDN}/embed/avatars/0.png`
-              }
-              size='sm'
-              aria-label='サーバーアイコン'
-            />
-            <span className='font-semibold'>{guild.name}</span>
-          </div>
+          <SingleSelectItem guild={guild} />
         </SelectItem>
       )}
     </Select>
+  );
+}
+
+function SingleSelectItem({
+  guild,
+  key,
+}: { guild?: RESTAPIPartialCurrentUserGuild | null; key?: Key }) {
+  return (
+    <div className='flex items-center gap-3 text-foreground' key={key}>
+      <Avatar
+        src={
+          guild?.icon
+            ? `${Discord.Endpoints.CDN}/icons/${guild.id}/${guild.icon}.webp`
+            : `${Discord.Endpoints.CDN}/embed/avatars/0.png`
+        }
+        size='sm'
+        aria-label='サーバーアイコン'
+      />
+      <span className='font-semibold'>{guild?.name}</span>
+    </div>
   );
 }
