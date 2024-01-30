7d261e0e3da5ac1414e1cae6682702c8b090d604
--global <--global>
2024-01-30 13:26:05 +0900

    Remove unnecessary code in SubmitButton function

--- src/app/(dashboard)/dashboard/guild/[guildId]/form-utils.tsx
+++ src/app/(dashboard)/dashboard/guild/[guildId]/form-utils.tsx
@@ -49,7 +49,7 @@ export function SwitchLabel({
 }
 
 export function SubmitButton() {
-  const { formState, reset } = useFormContext();
+  const { formState } = useFormContext();
 
   return (
     <div className='flex items-center gap-3 w-full pb-12'>
